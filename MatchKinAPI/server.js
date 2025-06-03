b4319928946f7284880bd75b9be715ccbcde9587
Tanishka-22 <tanishkashivhare22@gmail.com>
2025-06-03 22:55:28 +0530

    7

--- MatchKinAPI/server.js
+++ MatchKinAPI/server.js
@@ -14,7 +14,10 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [ 'https://match-kin.vercel.app', 'http://localhost:5173' ]
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
